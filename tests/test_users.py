cd2d948a185bf531ff3f7383465db4f47f225ed2
Isaac1502 <isaactadinagiraldo02@gmail.com>
2024-01-25 16:06:55 -0500

    conftest added

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,8 @@
 import pytest
+from jose import jwt
 
 from app import schemas
-from .database import client, session
+from app.config import settings
 
 
 def test_create_user(client):
@@ -13,8 +14,33 @@ def test_create_user(client):
     assert res.status_code == 201
 
 
-def test_login_user(client):
+def test_login_user(test_user, client):
     res = client.post(
-        "/login", data={"username": "test@test.com", "password": "password"}
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
     )
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, key=settings.secret_key, algorithms=[settings.algorithm]
+    )
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password", 403),
+        ("test@test.com", "wrongpassword", 403),
+        ("wrongemail@gmail.com", "wrongpassword", 403),
+        (None, "password", 422),
+        ("test@test.com", None, 422),
+    ],
+)
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
